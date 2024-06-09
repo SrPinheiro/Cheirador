8127fb474a049739c998b4336d70e5fc9248f064
srpinheiro <leonardogp.pinheiro@gmail.com>
2024-06-08 21:56:45 -0300

    feat: Estrutura BlueTooth adicionada.

--- tools/bluetooth.py
+++ tools/bluetooth.py
@@ -1,3 +1,6 @@
+from pybricks.messaging import BluetoothMailboxServer, Mailbox, BluetoothMailboxClient
+from configs.parametros import Parametros
+
 class BluetoothStatus:
     HOST = True
     CLIENT = False
@@ -7,4 +10,27 @@ class Bluetooth():
     HOST = BluetoothStatus.HOST
 
     def __init__(self, host):
-        self.souHost = host
+        if(host):
+            self.servidor = BluetoothMailboxServer()
+            self.mensageiro = Mailbox(Parametros.SERVER_BLUETOOTH_CODE, self.servidor)
+        else:
+            self.servidor  = BluetoothMailboxClient()
+            self.servidor.connect(Parametros.SERVER_BLUETOOTH_NAME)
+            self.mensageiro = Mailbox(Parametros.CLIENT_BLUETOOTH_CODE, self.servidor)
+
+    def aguardarConexao(self, conexoes=1):
+        self.servidor.wait_for_connections(conexoes)
+
+    def lerMensagem(self):
+        return self.mensageiro.read()
+    
+    def enviarMensagem(self, mensagem):
+        self.mensageiro.send(mensagem)
+
+    def aguardarMensagem(self):
+        self.mensageiro.wait()
+
+    def aguardarMensagemDiferente(self):
+        self.mensageiro.wait_new()
+        
+
